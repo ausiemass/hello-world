a020f8ae581e887d2e3c572a0a10dafcb45be8a5
ausiemass <ausiemass@yahoo.com>
2022-04-19 15:08:28 +0200

    bjjfvg
    ma

--- functions.php
+++ functions.php
@@ -4,12 +4,16 @@
  $num2 = 15;
 function calcAdd(&$num1, &$num2){
     $num2 = $num2 + 57;
+    $num1 = $num1 + 8;
     $sum = $num1 + $num2;
     return $sum;
 } 
 
 echo $sum;
+echo '<br>';
 
 echo $num1;
+echo '<br>';
 echo $num2;
+echo '<br>';
 ?>
